121e51261c8351166a794c2b456df4b2816999cc
axxe16 <axxe16@users.noreply.github.com>
2021-03-10 17:29:47 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,21 +1,3 @@
-<?php b4st_footer_before();?>
-
-<footer id="site-footer">
-
-  <div class="container">
-
-    <?php if(is_active_sidebar('footer-widget-area')): ?>
-    <div class="row pt-5 pb-4" id="footer" role="navigation">
-      <?php dynamic_sidebar('footer-widget-area'); ?>
-    </div>
-    <?php endif; ?>
-
-  </div>
-
-</footer>
-
-<?php b4st_footer_after();?>
-
 <?php b4st_bottomline();?>
 
 <!--
